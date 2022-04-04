d3a1802e70ed698067be53d17727786c9469967c
pagatelabirra <raulmatiascabral@gmail.com>
2022-04-04 19:39:00 -0300

    Continuacion de Primera entrega proyecto final

--- js/final.js
+++ js/final.js
@@ -19,11 +19,10 @@ function createProduct(name, size, colors, category, price, discount){
 
 //Creacion de un array de String con colores (retorna False si no se valido anteriormente el parametro de entrada)
 function createColors(num){
-    if(num != false){
         let colors = [];
         for(let i = 1; i <= num; i++){
-            while(colors.length() < i){
-                colors.push(stringControl(prompt("Ingrese un color : ")))
+            while(colors.length < i){
+                colors.push(repeat(stringControl(prompt("Ingrese un color : "))))
                 if(colors[i-1] === false){
                     console.clear()
                     console.log("Valor incorrecto, intente nuevamente.")
@@ -32,8 +31,13 @@ function createColors(num){
             }
         }
         return colors
-    }
-    return false
+}
+
+// Funcion Control String (no numero)
+function stringControl(val){
+    if(!isNaN(parseInt(val))){
+        return false
+    }return val
 }
 
 //Funcion control del numero de opciones
@@ -44,29 +48,23 @@ function optionControl(num){
     return false
 }
 
-//Funcion control de Strings
-function stringControl(str){
-    if (typeof str === "string"){
-        return str
-    }
-    return false
-}
-
 //Funcion control de Numeros
 function intControl(num){
         if(!isNaN(num) && num > 0){
             return num
         }
-        console.log("El valor ingresado es incorrecto.");
         return false
 }
 
 // Problema !! (Intento de Funcion recursiva generica para todas las funciones de control)
-function repeat(value, fun){
-    if(value === false){
-        console.log("Intenta otra vez ...")
-        while(value === false){
-            let value = fun // Funciona asi ?
+function repeat(fun){
+    let value = false
+    while(value === false){
+        value = fun
+        if(value === false){
+            console.log("El valor ingresado es incorrecto, intenta de nuevo por favor ...")
+            continue
+        }else{
             return value
         }
     }
@@ -91,14 +89,15 @@ function main(){
     while(option != 6){
         option = optionControl(parseInt(prompt("Ingresa una de las opciones listadas : ")))
         if(option === 1){
+            console.clear()
             let numProducts = intControl(parseInt(prompt("Ingresa la cantidad de productos a ingresar : ")))
-            while(numProducts != false && numProducts >= products.length()){
-                let name = stringControl(prompt("Ingrese el nombre del producto : "))
-                let size = intControl(parseInt(prompt("Ingrese el talle del producto : ")))
-                let colors = createColors(intControl(parseInt(prompt("Ingrese la cantidad de Colores que el producto va a tener : "))))
+            while(numProducts != false && numProducts >= products.length){
+                let name = prompt("Ingrese el Nombre del Producto : ")
+                let size = repeat(intControl(parseInt(prompt("Ingrese el talle del producto"))))
+                let colors = createColors(repeat(intControl(parseInt(prompt("Ingrese la cantidad de Colores que el producto va a tener : ")))))
             }
         }
-        console.log("El valor ingresado es incorrecto.")
+        console.log("El valor ingresado es incorrecto.");
     }
 
 }
